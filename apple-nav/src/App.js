0e92684643196d7960efccf86bae785a7f64bb3f
Anthony Sutton <anthony.sutton95@gmail.com>
2019-10-14 13:41:00 -0700

    Installed styled components, started to build nav wrapper

---
+++ apple-nav/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import NavWrapper from "./Components/NavWrapper";
+
+function App() {
+  return (
+    <div className="App">
+      <NavWrapper />
+    </div>
+  );
+}
+
+export default App;
---
+++ apple-nav/src/Components/NavWrapper.js
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+
+const NavWrapper = () => {
+  return (
+    <div>
+      <div className="nav-links">
+        <a href="#">Apple</a>
+        <a href="#">Mac</a>
+        <a href="#">iPad</a>
+        <a href="#">iPhone</a>
+        <a href="#">Watch</a>
+        <a href="#">TV</a>
+        <a href="#">Music</a>
+        <a href="#">Support</a>
+      </div>
+    </div>
+  );
+};
+
+export default NavWrapper;
